16a1fa2a35b776fd1b950b5a406b95b380daa612
Gaurish Garg <gaurish.garg.cool@gmail.com>
2023-01-25 14:25:39 +0530

    deploying to cyclic check

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const https = require('https');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const path = require('path');
-const hbs = require('hbs');
+const cors = require('cors');
 
 var x = 5;
 dotenv.config({
@@ -13,28 +13,20 @@ var myport = {PORT: process.env.PORT||3000};
     console.log(myport.PORT);
 
 app = express();
-app.set('view engine', 'hbs');
-app.set('views',path.join(__dirname, '/views'));
 app.use(bodyParser.urlencoded({
     extended: true
 })); //express uses bodyparser
-// app.use(function(req,res,next){
-//     res.setHeader("Content-Encoding", "gzip");
-//     next();
-// });
-app.use(express.static(path.join(__dirname, '/public')));
-app.get('/', function (req, res){    
-    // res.setHeader("Content-Type", "application/json");
-    // res.setHeader("Content-Type", "application/wasm");
-    // res.setHeader("Content-Encoding", "gzip");
-    res.render('game.hbs',myport);
 
+app.use(express.static(path.join(__dirname, '/')));
+app.get('/', function (req, res){   
+    res.send("Hi! This is server end");
 
 });
-app.post('/saveresponse',async function(req,res){
+app.post('/saveresponse',cors(), async function(req,res){
     console.log(req.body);
     x = 10;
-    res.redirect("/responsesaved");
+    res.send(req.body);
+    // res.redirect("/responsesaved");
 });
 app.get('/responsesaved', async function(req,res){
     console.log("Communication success");
@@ -43,4 +35,4 @@ app.get('/responsesaved', async function(req,res){
 });
 module.exports = app;
 
-console.log("PATH = "+path.join(__dirname, '/game.hbs'));
+
