02e6e2cd8c5c9732543fa750761ba7bf6189bda2
Gaurish Garg <gaurish.garg.cool@gmail.com>
2023-01-25 13:23:34 +0530

    updating build

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const https = require('https');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const path = require('path');
+const hbs = require('hbs');
+
+var x = 5;
+dotenv.config({
+    path: './.env'
+});
+var myport = {PORT: process.env.PORT||3000};    
+    console.log(myport.PORT);
+    express.static.mime.define({'application/wasm': ['wasm']})
+app = express();
+app.set('view engine', 'hbs');
+app.set('views',path.join(__dirname, '/views'));
+app.use(bodyParser.urlencoded({
+    extended: true
+})); //express uses bodyparser
+// app.use(function(req,res,next){
+//     res.setHeader("Content-Encoding", "gzip");
+//     next();
+// });
+app.use(express.static(path.join(__dirname, '/public')));
+app.get('/', function (req, res){    
+    // res.setHeader("Content-Type", "application/json");
+    // res.setHeader("Content-Type", "application/wasm");
+    // res.setHeader("Content-Encoding", "gzip");
+    res.render('game.hbs',myport);
+
+
+});
+app.post('/saveresponse',async function(req,res){
+    console.log(req.body);
+    x = 10;
+    res.redirect("/responsesaved");
+});
+app.get('/responsesaved', async function(req,res){
+    console.log("Communication success");
+    res.send("Communication Successful"+x);
+
+});
+module.exports = app;
+
+console.log("PATH = "+path.join(__dirname, '/game.hbs'));
